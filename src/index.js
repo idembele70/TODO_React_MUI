339dcc854105e3a45c6c61fbffd23597e5450776
idembele70 <idembele70@gmail.com>
2021-07-02 00:32:21 +0200

    first change

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Title from "./components/title";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Title>TODO APP</Title>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
